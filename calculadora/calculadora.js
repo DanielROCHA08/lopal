f99555b8a2d98e44f4e3ba26b7deadfc8b2839ee
DanielRRocha <dani.rocha3045@gmail.com>
2026-03-04 16:50:51 -0300

    feat: Opções de encerramento da calculadora

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,9 +1,11 @@
-function calculadora(){
+function calculadora(){"sim"
     while(true){
         let resultado = 0.0;
         let operando1;
         let operando2;
         let operador;
+        let continua;
+        let operador_valido = true;
 
         operando1 = parseFloat( prompt("Digite o primeiro número: ") );
         operando2 = parseFloat( prompt("Digite o segundo número: ") );
@@ -16,13 +18,24 @@ function calculadora(){
         } else if ( operador === "*" ){
             resultado = operando1 * operando2;
         } else if ( operador === "/"){
+            if (operando2 == 0){
+                alert("Não é possível dividir por zero.");
+                continua = prompt("Digite sim para continuar e não para encerrar");
+                if ( continua === "não"){
+                    return
+                } 
+            } else { 
             resultado = operando1 / operando2;
+            }
         } else {
-            alert("Operador inválido");
+            alert("Operador Inválido");
+            operador_valido = false;
+            continua = prompt("Dgite sim para continuar e não para encerrar");
             return
         }
-        
+        if( operador != "/" || operando2 != 0 ){
         alert("Resultado:     " +
                 operando1 + " " + operador + " " + operando2 + " = " + resultado);
+        }
     }        
 }
