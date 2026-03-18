c5a6850520efad5c6b6d5ac565f74a7505d22703
DanielRRocha <dani.rocha3045@gmail.com>
2026-03-18 16:50:26 -0300

    feat: Urna e Procedimento

---
+++ procedimento/procedimento.js
@@ -0,0 +1,30 @@
+function procedimento(){
+
+    
+    function inicio(){
+          calcular(a = 3, b = 4);
+        mensagem(frase = "O resultado de a² + b² é : " +  calcular(a = 3, b = 4));
+    }
+
+     
+
+    function mensagem(frase){
+        let linha = "-";
+        let i = 0;
+         
+
+        do{
+            linha = linha + "-"
+            i++;
+        }while( i < 50 );
+        
+        alert( linha + "\n" + frase + "\n" + linha);
+    }
+
+    function calcular (a, b){
+        return    a * a + b * b;
+        
+    }
+ inicio();
+
+}
